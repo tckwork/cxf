a41c21a8f67992b835731ca96f4ed206ac0305b0
David Blevins <david.blevins@gmail.com>
2021-04-15 05:03:34 -0700

    Pipe all output to disk

--- 3.4.x-fixes.sh
+++ 3.4.x-fixes.sh
@@ -2,8 +2,11 @@
 
 ##[ Imports ]####################
 
+{
 source "$(dirname $0)/utils.sh"
 
 export CXF_VERSION="$(version 3.4.x-fixes)"
 
 ./runtests "$@"
+} 2>&1 | tee 3.4.x-fixes.log
+
--- master.sh
+++ master.sh
@@ -2,8 +2,11 @@
 
 ##[ Imports ]####################
 
+{
 source "$(dirname $0)/utils.sh"
 
 export CXF_VERSION="$(version master)"
 
 ./runtests "$@"
+} 2>&1 | tee master.log
+
